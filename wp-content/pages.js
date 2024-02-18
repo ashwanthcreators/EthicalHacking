76398fa7e4914999ed4ad9572d111b4a067ac684
ashwanthcreators <85355780+ashwanthcreators@users.noreply.github.com>
2024-02-18 13:56:52 +0530

    Add files via upload

---
+++ wp-content/pages.js
@@ -0,0 +1,25 @@
+    var authenticated = sessionStorage.getItem("authenticated");
+
+    if (!authenticated) {
+        // Redirect back to login.html if not authenticated
+        window.location.href = "login.html";
+    }
+
+    function copyCommandsToClipboard() {
+        const commandElements = document.querySelectorAll('.command');
+        const commandsText = Array.from(commandElements).map(element => element.innerText).join('\n');
+
+        const tempTextArea = document.createElement('textarea');
+        tempTextArea.value = commandsText;
+
+        document.body.appendChild(tempTextArea);
+
+        tempTextArea.select();
+        tempTextArea.setSelectionRange(0, 99999);
+
+        document.execCommand('copy');
+
+        document.body.removeChild(tempTextArea);
+
+        alert('Commands copied to clipboard!');
+    }
